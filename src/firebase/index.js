24a85a9849ef0405ea1ae7dfc9c8df46fb8417b2
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2022-03-11 18:11:31 +0500

    Started Working on the backend

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -5,7 +5,13 @@ import 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
- 
+    apiKey: "AIzaSyDrA7W6ktKShzVNQ9z5NSTkXPMF-sEDY_o",
+    authDomain: "teamovercsarchitect.firebaseapp.com",
+    projectId: "teamovercsarchitect",
+    storageBucket: "teamovercsarchitect.appspot.com",
+    messagingSenderId: "655901470112",
+    appId: "1:655901470112:web:808f81ecd47d4f204d88c5",
+    measurementId: "G-15LD304GVS"
 };
 
 // Initialize Firebase
